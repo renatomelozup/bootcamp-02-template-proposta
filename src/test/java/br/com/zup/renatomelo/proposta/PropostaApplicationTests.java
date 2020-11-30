0f10de7c492bd79f33f072ff60c6b28587be3fd9
Renato Pessoa <renato.melo@zup.com.br>
2020-11-30 17:16:08 -0300

    Initial commit

---
+++ src/test/java/br/com/zup/renatomelo/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.renatomelo.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
