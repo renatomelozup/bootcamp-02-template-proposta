0d54bbd3590c8e4d50fa25c860ae54129062f84a
Renato Pessoa <renato.melo@zup.com.br>
2020-12-10 20:30:04 -0300

    050 - SecurityConfiguration

---
+++ src/main/java/br/com/zup/renatomelo/proposta/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.zup.renatomelo.proposta.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.POST, "/api/v1/dados/**").hasAuthority("SCOPE_proposta:write")
+                        .antMatchers(HttpMethod.POST, "/api/v1/biometrias/**").hasAuthority("SCOPE_biometrias:write")
+                        .antMatchers(HttpMethod.GET, "/api/v1/dados/**").hasAuthority("SCOPE_proposta:read")
+                        .antMatchers(HttpMethod.GET, "/actuator/health/**").hasAuthority("SCOPE_proposta:read")
+                        .anyRequest().authenticated()
+        )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
