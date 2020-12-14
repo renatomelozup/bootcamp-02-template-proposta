5e819ca3a8286fc32d05807882bfa5d06f7fa28c
Renato Pessoa <renato.melo@zup.com.br>
2020-12-14 12:06:05 -0300

    055 - wip

--- src/main/java/br/com/zup/renatomelo/proposta/proposta/controller/CartaoController.java
+++ src/main/java/br/com/zup/renatomelo/proposta/proposta/controller/CartaoController.java
@@ -1,20 +1,23 @@
 package br.com.zup.renatomelo.proposta.proposta.controller;
 
 import br.com.zup.renatomelo.proposta.advice.ApiErrorException;
+import br.com.zup.renatomelo.proposta.proposta.model.Biometria;
 import br.com.zup.renatomelo.proposta.proposta.model.Cartao;
 import br.com.zup.renatomelo.proposta.proposta.model.CartaoBloqueio;
+import br.com.zup.renatomelo.proposta.proposta.repository.BiometriaRepository;
 import br.com.zup.renatomelo.proposta.proposta.repository.CartaoBloqueioRepository;
 import br.com.zup.renatomelo.proposta.proposta.repository.CartaoRepository;
+import br.com.zup.renatomelo.proposta.proposta.request.NovaBiometriaRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,6 +29,8 @@ public class CartaoController {
     private CartaoRepository cartaoRepository;
     @Autowired
     private CartaoBloqueioRepository cartaoBloqueioRepository;
+    @Autowired
+    private BiometriaRepository biometriaRepository;
 
     @PostMapping("/{id}/bloquear")
     @Transactional
@@ -57,5 +62,29 @@ public class CartaoController {
         cartaoRepository.save(cartaoNoBanco);
 
         return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/{id}/biometria")
+    @Transactional
+    public ResponseEntity<?> cadastrarBiometria (@PathVariable UUID id,
+                                        @RequestBody @Valid NovaBiometriaRequest biometria,
+                                        UriComponentsBuilder uriComponentsBuilder) {
+
+        Optional<Cartao> cartao = cartaoRepository.findById(id);
+        if(cartao.isEmpty()) {
+            throw new ApiErrorException(HttpStatus.NOT_FOUND, "registro não encontrado");
+        }
+
+
+        Biometria novaBiometria = biometria.toModel();
+        novaBiometria.setCartao(cartao.get());
+        biometriaRepository.save(novaBiometria);
+
+        cartao.get().getBiometriaList().add(novaBiometria);
+
+        URI uri = uriComponentsBuilder.path("/api/v1/cartoes/{id}/biometria").buildAndExpand(novaBiometria.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
